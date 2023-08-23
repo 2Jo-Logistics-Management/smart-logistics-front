16a08df5474de5f100313e12a17351c4aed276c0
ap9407 <ap9407@naver.com>
2023-08-23 10:37:59 +0900

    fix: memberReducer삭제 #17

--- frontend/src/redux/reducer.js
+++ frontend/src/redux/reducer.js
@@ -6,7 +6,6 @@ import pOrderListReducer from './slices/pOrderListReducer'
 import receiveListReducer from './slices/receiveListReducer'
 import warehouseListReducer  from './slices/warehouseListReducer';
 import loginResponseReducer from './slices/loginResponseReducer';
-import memberListReducer from './slices/memberListReducer';
 
 const rootReducer = combineReducers({
     porderModal: porderModalDuck,
@@ -16,7 +15,6 @@ const rootReducer = combineReducers({
     receiveList: receiveListReducer,
     warehouseList: warehouseListReducer,
     loginResponse: loginResponseReducer,
-    memberList: memberListReducer
 });
 
 
