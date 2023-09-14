3ef6e6fdda086a8960699f4fa0ad36e2d34a8737
kaf829@naver.com <kaf829@naver.com>
2023-09-14 20:44:10 +0900

    feat: 발주품목삭제 #34

---
+++ frontend/src/axios/pOrderItemsDeleteAxios.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import swal from "sweetalert2";
+axios.defaults.withCredentials = true;
+//발주리스트에서 delete하면 통신
+const pOrderItemsDeleteAxios = (pOrderCodes,pOrderItemList) => {
+  alert(pOrderCodes+ "  리스트:"+pOrderItemList);
+ 
+  axios
+    .delete(`http://localhost:8888/api/porder-item/delete?pOrderCode=${pOrderCodes}&pOrderItemNo=${pOrderItemList}`)
+    .then(() => {
+      swal.fire({
+        title: "삭제 완료",
+        text: "재고가 삭제되었습니다.",
+        icon: "success",
+      });
+    })
+    .catch((error) => {
+      swal.fire({
+        title: "삭제 실패",
+        text: `Error: ${error.message}`,
+        icon: "error",
+      });
+    });
+};
+
+export default pOrderItemsDeleteAxios;
