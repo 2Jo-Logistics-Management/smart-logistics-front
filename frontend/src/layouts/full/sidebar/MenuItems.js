d69a738f0e8022ec862c77b5b2ddefcb384f32db
kaf829@naver.com <kaf829@naver.com>
2023-09-14 20:35:10 +0900

    feat; 인사관리시스템 권한부여 #34

--- frontend/src/layouts/full/sidebar/MenuItems.js
+++ frontend/src/layouts/full/sidebar/MenuItems.js
@@ -1,8 +1,9 @@
+import { useSelector } from 'react-redux';
 import {
-   IconCopy, IconLayoutDashboard, IconTypography, IconSun
+  IconCopy, IconLayoutDashboard, IconTypography, IconSun
 } from '@tabler/icons';
 import { uniqueId } from 'lodash';
-//사이드바 종류(주소 바꾸고싶으면 여기서 바꾸면 됨)
+import { useMemo } from 'react';
 
 const subheaderStyle = {
   fontSize: '14px',
@@ -13,63 +14,68 @@ const subheaderStyle = {
   textTransform: 'uppercase',
 };
 
-const Menuitems = [
-  {
-    navlabel: true,
-    subheader: <div style= {subheaderStyle} >재고관리시스템</div>,
-  },
-  {
-    id: uniqueId(),
-    title: '메인페이지',
-    icon: IconLayoutDashboard,
-    href: '/dashboard',
-  },
-  {
-    id: uniqueId(),
-    title: '품목 관리시스템',
-    icon: IconSun,
-    href: '/item/list',
-  },
-  {
-    id: uniqueId(),
-    title: '재고 등록/수정/삭제',
-    icon: IconLayoutDashboard,
-    href: '/warehouse/list',
-  },
-  {
-    navlabel: true,
-    subheader: <div style= {subheaderStyle}>물류관리시스템</div>,
-  },
-  {
-    id: uniqueId(),
-    title: '거래처 관리시스템',
-    icon: IconTypography,
-    href: '/Logistic/Account',
-  },
-  {
-    id: uniqueId(),
-    title: '발주 업무시스템',
-    icon: IconCopy,
-    href: '/Logistic/POrder',
-  },
-  {
-    id: uniqueId(),
-    title: '입고 업무시스템',
-    icon: IconCopy,
-    href: '/Logistic/Receive',
-  },
- 
-  {
-    navlabel: true,
-    subheader: <div style= {subheaderStyle} >인사관리시스템</div>,
-  },
-  {
-    id: uniqueId(),
-    title: '사원 관리 시스템',
-    icon: IconCopy,
-    href: '/member/list',
-  },
-  
-];
 
-export default Menuitems;
+
+const MenuItems = () => {
+  const memberData = useSelector((state) => state.memberData.memberData);
+  const memberRole = memberData?.data?.memberRole || "";
+
+  const items = useMemo(() => [
+    {
+      navlabel: true,
+      subheader: <div style={subheaderStyle}>재고관리시스템</div>,
+    },
+    {
+      id: uniqueId(),
+      title: '메인페이지',
+      icon: IconLayoutDashboard,
+      href: '/dashboard',
+    },
+    {
+      id: uniqueId(),
+      title: '품목 관리시스템',
+      icon: IconSun,
+      href: '/item/list',
+    },
+    {
+      id: uniqueId(),
+      title: '창고관리시스템',
+      icon: IconLayoutDashboard,
+      href: '/warehouse/list',
+    },
+    {
+      navlabel: true,
+      subheader: <div style={subheaderStyle}>물류관리시스템</div>,
+    },
+    {
+      id: uniqueId(),
+      title: '발주 관리시스템',
+      icon: IconCopy,
+      href: '/Logistic/POrder',
+    },
+    {
+      id: uniqueId(),
+      title: '입고 관리시스템',
+      icon: IconCopy,
+      href: '/Logistic/Receive',
+    },
+    
+    ...(memberRole === 'ADMIN' ? [
+      {
+        navlabel: true,
+        subheader: <div style={subheaderStyle}>인사관리시스템</div>,
+      },
+      {
+        id: uniqueId(),
+        title: '사원 관리 시스템',
+        icon: IconCopy,
+        href: '/member/list',
+      }
+    ] : []),
+  ], [memberRole]); // `memberRole`이 변경될 때만 `items` 배열을 재생성
+
+  return items;
+};
+
+
+export default MenuItems;
