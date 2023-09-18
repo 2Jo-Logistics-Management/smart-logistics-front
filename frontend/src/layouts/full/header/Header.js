0ddb03ca4b4117ac757222ebbe9680363ed312a6
ap9407 <ap9407@naver.com>
2023-09-18 10:29:04 +0900

    fix: 거래처 화면 되돌리기 #23

--- frontend/src/layouts/full/header/Header.js
+++ frontend/src/layouts/full/header/Header.js
@@ -37,7 +37,7 @@ const Header = (props) => {
         if(response.data.success) {
           dispatch(close())
           navigate("/auth/login");  
-          
+          localStorage.clear();
         }
       })
     } catch (error) {
