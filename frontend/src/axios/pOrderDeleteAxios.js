fa0566ea1884d9ddd54f8fa5571c503168b0d084
kaf829@naver.com <kaf829@naver.com>
2023-09-20 01:11:49 +0900

    feat: 발주삭제 알림창 시점 수정 #52

--- frontend/src/axios/pOrderDeleteAxios.js
+++ frontend/src/axios/pOrderDeleteAxios.js
@@ -2,27 +2,27 @@ import axios from "axios";
 import swal from "sweetalert2";
 axios.defaults.withCredentials = true;
 
-const pOrderDeleteAxios = async (pOrderCodes) => {
+const pOrderDeleteAxios = async (pOrderCodes) => { 
   try {
-    await axios.delete(`http://localhost:8888/api/porder/delete?pOrderCodes=${pOrderCodes}`);
+    const response = await axios.delete(`http://localhost:8888/api/porder/delete?pOrderCodes=${pOrderCodes}`);
     // If the request is successful, this code will execute
-    swal.fire({
+   
+    await swal.fire({
       title: "삭제 완료",
-      text: "재고가 삭제되었습니다.",
+      text: "발주가 삭제되었습니다",
       icon: "success",
     });
   } catch (error) {
-    // If there's an error, this code block will execute
     if (error.response && error.response.status === 400) {
       swal.fire({
         title: "삭제 실패",
-        text: "발주가 진행된 이후로는 삭제할 수 없습니다.",
+        text: "진행이 된 발주는 삭제할 수 없습니다",
         icon: "error",
       });
     } else {
-      // Handle other errors
       console.error("An error occurred:", error);
     }
+    
   }
 };
 
